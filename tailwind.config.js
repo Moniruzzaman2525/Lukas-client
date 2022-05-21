255eb6f7f96a819b8b33d0e1f52465ee9d915b72
unknown <md616264@gmail.com>
2022-05-22 00:01:20 +0600

    start assignment

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ], daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#176F6B",
+
+          "secondary": "#FFC000",
+
+          "accent": "#FFEDD5",
+
+          "neutral": "#F3F4F6",
+
+          "base-100": "#FAF7F5",
+
+          "info": "#fff2f2",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
