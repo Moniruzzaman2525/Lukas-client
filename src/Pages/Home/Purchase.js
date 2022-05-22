3029fdcf4c9784eebd06f9c46d3c7e6bb6de273e
unknown <md616264@gmail.com>
2022-05-22 23:19:15 +0600

    purchase page start

--- src/Pages/Home/Purchase.js
+++ src/Pages/Home/Purchase.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Purchase = () => {
-    const { id } = useParams()
+    const { id } = useParams();
+    const [service, setService] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/services/${id}`)
+            .then(res => res.json())
+            .then(data => setService(data))
+    }, [])
     return (
         <div>
-            <h1>{id}</h1>
+            <div class="mx-auto card w-96 bg-base-100 shadow-xl">
+                <figure class="px-10 pt-10">
+                    <img src={service.image} alt="Shoes" class="rounded-xl" />
+                </figure>
+                <div class="card-body items-center text-center">
+                    <h2 class="card-title">{service.name}</h2>
+                    <p>{service.description}</p>
+                    <p>Price: ${service.price}</p>
+                    <p>InStock: {service.inStock}</p>
+                    <p>Minimum Quantity: {service.mnQuantity}</p>
+                </div>
+            </div>
         </div>
     );
 };
